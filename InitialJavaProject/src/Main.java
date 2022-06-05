3d0af81c5df0afaa17f2dd5dcc8eaf164b2975d7
ars1208 <ara.kaz1208@gmail.com>
2022-06-05 18:59:13 +0900

    Initial commit

---
+++ InitialJavaProject/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String word = "Hello World";
+        int sum = 2 + 3;
+        System.out.println(word);
+        System.out.println(sum);
+    }
+}
